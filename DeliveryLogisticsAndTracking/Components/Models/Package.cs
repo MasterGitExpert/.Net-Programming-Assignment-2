a5ba8d6c4225a14d78c9c292b3290c55932d76be
Annabel <annabel.phu@hotmail.com>
2025-10-20 23:33:47 +1100

    add google maps

---
+++ DeliveryLogisticsAndTracking/Components/Models/Package.cs
@@ -0,0 +1,14 @@
+﻿using Radzen;
+
+namespace DeliveryLogisticsAndTracking.Components.Models
+{
+    public class Package
+    {
+        public string Name { get; set; } = string.Empty;
+        public GoogleMapPosition Destination { get; set; } = new();
+        public string Address { get; set; } = string.Empty;
+        public string Status { get; set; } = "Pending"; // e.g., Pending, In Transit, Delivered
+        public DateTime? EstimatedDeliveryTime { get; set; }
+    }
+
+}
--- DeliveryLogisticsAndTracking/obj/Debug/net9.0/DeliveryLogisticsAndTracking.AssemblyInfo.cs
+++ DeliveryLogisticsAndTracking/obj/Debug/net9.0/DeliveryLogisticsAndTracking.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeliveryLogisticsAndTracking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f52953e30346752651ab879c6761b0ae2b651679")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+555bdef91191f9fce0e66ea05a5b683a83cc5734")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeliveryLogisticsAndTracking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeliveryLogisticsAndTracking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
